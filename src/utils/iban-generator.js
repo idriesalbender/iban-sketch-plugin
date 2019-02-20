a00a72114d4c85db40473a440f3210ccbd49c6e3
idriesalbender <idries_albender@hotmail.com>
2019-02-20 17:31:17 +0100

    Refactor iban-generation

--- src/utils/iban-generator.js
+++ src/utils/iban-generator.js
@@ -1,46 +1,79 @@
 import DATA from './data.json';
-
 const Chance = require('chance');
 const chance = new Chance();
 
-const CHAR_MAP = {
-  a: {
-    key: 'alpha',
-    getString: length => chance.string({ length, pool: 'ABCDEFGHIJKLMNOPQRSTUVW' }),
-  },
-  c: {
-    key: 'mixed',
-    getString: length =>
-      chance.string({
-        length,
-        pool: 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789',
-      }),
-  },
-  n: {
-    key: 'digit',
-    getString: length => chance.string({ length, pool: '0123456789' }),
-  },
+const POOLS = {
+  ALPHA: 'ABCDEFGHIJKLMNOPQRSTUVW',
+  DIGIT: '0123456789',
+  MIXED: 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789',
 };
 
-const COUNTRY_RANGES = {
-  EU: 'EU',
-  EAC: 'EAC',
-  EUROZONE: 'EUROZONE',
-  WORLD: 'WORLD',
+/**
+ * Test if string is alpha character
+ * @param {string} str - string to test
+ * @returns {mixed}
+ */
+export const isAlpha = str => {
+  const match = str.match(/(\d+)(?=a)/);
+  if (match) return parseInt(match[0]);
+  return false;
 };
+
+/**
+ * Test if string is digit character
+ * @param {string} str - string to test
+ * @returns {mixed}
+ */
+export const isDigit = str => {
+  const match = str.match(/(\d+)(?=n)/);
+  if (match) return parseInt(match[0]);
+  return false;
+};
+
+/**
+ * Test if string is mixed character
+ * @param {string} str - string to test
+ * @returns {mixed}
+ */
+export const isMixed = str => {
+  const match = str.match(/(\d+)(?=c)/);
+  if (match) return parseInt(match[0]);
+  return false;
+};
+
+/**
+ * Generates a random string at specific length from specific pool
+ * @param {int} length - length of string to generate
+ * @param {string} pool - pool of chars to choose from
+ */
+const makeString = (length, pool) => chance.string({ length, pool });
 
 /**
  * Generates an IBAN number for a specified country
  * @param {string} countryName - name of country to generate IBAN for
  * @returns {string}
  */
-const generateIBANFromCountry = countryName => {
+export const generateIBANFromCountry = countryName => {
+  if (!countryName) {
+    throw new Error('No country string provided!');
+  }
+
   const result = DATA.find(item => item.country.name === countryName);
+
+  if (!result) {
+    throw new Error('Country not found for given string!');
+  }
+
   const { bbanFormat, country } = result;
 
   const ibanPrefix = `${country.code}${chance.string({ length: 2, pool: '0123456789' })}`;
-  const bbanSuffix = bbanFormat.order
-    .map(unit => CHAR_MAP[unit].getString(bbanFormat[CHAR_MAP[unit].key]))
+  const bbanSuffix = bbanFormat
+    .split(',')
+    .map(unit => {
+      if (isAlpha(unit)) return makeString(isAlpha(unit), POOLS.ALPHA);
+      if (isDigit(unit)) return makeString(isDigit(unit), POOLS.DIGIT);
+      if (isMixed(unit)) return makeString(isMixed(unit), POOLS.MIXED);
+    })
     .join('');
 
   return `${ibanPrefix}${bbanSuffix}`.match(/.{1,4}/g).join(' ');
@@ -61,10 +94,6 @@ export const generateIBAN = options => {
   if (!options) {
     pool = DATA.map(c => c.country.name);
   }
-  // TODO: implement trade association pools
-  // else if (options.range) {
-  //   pool = DATA.filter(c => c.country.range.find(options.range)).map(c => c.country.name);
-  // }
 
   if (pool) {
     result = pool[chance.integer({ min: 0, max: pool.length })];
