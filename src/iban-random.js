22c56f75b492bb5743fcc6536be5d1660ce03821
idriesalbender <idries_albender@hotmail.com>
2019-02-20 12:46:58 +0100

    Create random IBAN command

---
+++ src/iban-random.js
@@ -0,0 +1,21 @@
+import sketch, { DataSupplier, fromNative } from 'sketch';
+import util from 'util';
+import { generateIBAN } from './utils/iban-generator';
+
+export const onStartup = () => {
+  DataSupplier.registerDataSupplier('public.text', 'Random IBAN', 'SupplyRandomIBAN');
+};
+
+export const onShutdown = () => {
+  DataSupplier.deregisterDataSuppliers();
+};
+
+export const onSupplyRandomIBAN = context => {
+  const { data } = context;
+  const { key } = data;
+  const items = util.toArray(data.items).map(fromNative);
+
+  items.forEach((_, index) => {
+    DataSupplier.supplyDataAtIndex(key, generateIBAN(), index);
+  });
+};
